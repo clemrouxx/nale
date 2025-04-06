1ce48db9d0c72c0bd42fce1cc878e0233b2b16a0
clemrouxx <clemroux03@gmail.com>
2025-04-06 13:03:52 +0200

    small improvement for text styles

--- src/exportUtils.js
+++ src/exportUtils.js
@@ -2,6 +2,9 @@ import {
     $isTextNode,
   } from 'lexical';
 
+
+const TEXT_FORMAT_COMMANDS = {bold:"\\textbf",italic:"\\textit",capitalize:"\\textsc"}
+
 function putInCommand(string,command){
     return `${command}{${string}}`;
 }
@@ -21,7 +24,9 @@ function convertToLatex(node){
     }
     if ($isTextNode(node)) {
         string += node.getTextContent();
-        if (node.hasFormat("bold")) string = putInCommand(string,"\\textbf");
+        for (const format in TEXT_FORMAT_COMMANDS){
+            if (node.hasFormat(format)) string = putInCommand(string,TEXT_FORMAT_COMMANDS[format]);
+        }
     }
     else{
         string += node.getChildren().map(convertToLatex).join(' ');
