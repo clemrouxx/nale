5afc1cc6485532e7f85d0d908a011876db4ccc05
clemrouxx <clemroux03@gmail.com>
2025-05-22 14:14:53 +0200

    added basic zoom capabilities

--- src/ActionBar.jsx
+++ src/ActionBar.jsx
@@ -1,13 +1,36 @@
 import {$getRoot} from 'lexical';
 import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
 import {convertToLatex} from './plugins/LatexExportPlugin/latexUtils';
+import { useEffect, useState } from 'react';
+import { setGlobalCSSRule } from './utils/generalUtils';
+
+const zoomFactors = [
+  0.25,
+  0.33,
+  0.5,
+  0.67,
+  0.75,
+  0.8,
+  0.9,
+  1,
+  1.1,
+  1.25,
+  1.5,
+  1.75,
+  2,
+  2.5,
+  3,
+  4,
+  5
+];
+
 
 export const ActionBar = () => {
     return (
         <div className="span2cols actionbar">
             <h1 className='inline'>NaLE</h1>
             <ExportButton/>
-            <button>Test</button>
+            <DisplayMenu/>
         </div>
     )
 }
@@ -27,4 +50,27 @@ const ExportButton = () => {
             <button onClick={readEditorState}>Export</button>
         </>
     );
+}
+
+const DisplayMenu = () => {
+    const [zoomLevel,setZoomLevel] = useState(9);
+
+    useEffect(()=>{
+        setGlobalCSSRule(".editor-base","--editor-scale",zoomFactors[zoomLevel]);
+    },[zoomLevel]);
+
+    const zoomIn = () => {
+        if (zoomLevel<zoomFactors.length-1) setZoomLevel(zoomLevel+1);
+    }
+
+    const zoomOut = () => {
+        if (zoomLevel>=1) setZoomLevel(zoomLevel-1);
+    }
+
+    return (<>
+        
+        <button onClick={zoomIn}>Zoom +</button>
+        <button onClick={zoomOut}>Zoom -</button>
+        {`${Math.round(100*zoomFactors[zoomLevel])} %`}
+    </>)
 }
